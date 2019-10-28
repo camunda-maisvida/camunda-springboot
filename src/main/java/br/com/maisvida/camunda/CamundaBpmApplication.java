d0e24aa3b514792fe43565a76e5a5ea8254e0c25
Rodolfo Cruz <rodolfocruz.ti@gmail.com>
2019-10-28 18:03:44 -0300

    Initial commit

---
+++ src/main/java/br/com/maisvida/camunda/CamundaBpmApplication.java
@@ -0,0 +1,13 @@
+package br.com.maisvida.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaBpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaBpmApplication.class, args);
+	}
+
+}
